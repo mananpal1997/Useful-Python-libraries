4c73d633a1fe9bc1894143a2e501970c58d2ffe9
mananpal1997 <brunodicosta@gmail.com>
2016-06-04 12:14:21 +0530

    Generate animated string art

---
+++ WORDCLOUD/wordcloud_test.py
@@ -0,0 +1,23 @@
+from os import path
+from wordcloud import WordCloud
+
+d = path.dirname(__file__)
+
+# Read the whole text.
+text = open(path.join(d, 'sample.txt')).read()
+
+# Generate a word cloud image
+wordcloud = WordCloud().generate(text)
+
+# Display the generated image:
+# the matplotlib way:
+import matplotlib.pyplot as plt
+plt.imshow(wordcloud)
+plt.axis("off")
+
+# take relative word frequencies into account, lower max_font_size
+wordcloud = WordCloud(max_font_size=40, relative_scaling=.5).generate(text)
+plt.figure()
+plt.imshow(wordcloud)
+plt.axis("off")
+plt.show()
